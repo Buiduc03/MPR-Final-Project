bb9ae0ea1524274b7653083e6f395f4578e23e43
Buiduc03 <116989742+Buiduc03@users.noreply.github.com>
2024-04-18 13:02:43 +0700

    Update firebase.js

--- util/firebase.js
+++ util/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBae9o1bR-Rm3xlV74ON2VVTuHhf9dHxU8",
-    authDomain: "react-native-course-2f9bc.firebaseapp.com",
-    databaseURL: "https://react-native-course-2f9bc-default-rtdb.firebaseio.com",
-    projectId: "react-native-course-2f9bc",
-    storageBucket: "react-native-course-2f9bc.appspot.com",
-    messagingSenderId: "484348690065",
-    appId: "1:484348690065:web:e56dde4a6ed3c347f74930"
-};
+
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
